0bd9b028011152a79817ddf14c0069be0add3407
Albert Boehmler <ajboehmler@gmail.com>
2024-10-26 16:33:18 -0400

    v1 of MW markers summary doc

--- PIECES/mw-markers-summary.tsx
+++ PIECES/mw-markers-summary.tsx
@@ -1,9 +1,56 @@
+import { ReactNode } from 'react';
+import { HEX_COLOR_CODE_ATTACK_COUNTER, HEX_COLOR_CODE_MANA_COUNTER, HEX_COLOR_CODE_MANALITH_TOKEN, HEX_COLOR_CODE_MANAWAVE_ROUND_TOKEN, HEX_COLOR_CODE_POPULATION_INCREASE_COUNTER, HEX_COLOR_CODE_POPULATION_SACRIFICE_COUNTER, HEX_COLOR_CODE_POPULATION_TOKEN, HEX_COLOR_CODE_SHIELD_COUNTER, HEX_COLOR_CODE_SOULSTAIN_TOKEN } from './constants';
+import { createImgComponentFromTag } from './icons';
 import './mw-markers-summary-styles.css';
 
 
-export const MwMarkersSummarySheet = () => {
 
-  const markerItems = Array.from({ length: 30 }, (_, index) => index + 1); 
+const MARKER_SUMMARY_VIEW_ICON_SIZE = 24;
+const MARKER_SUMMARY_COLOR_BLOCK_SIZE = 24;
+
+const ManaCounterSummaryIcon = () => createImgComponentFromTag('<::mana-counter::>', MARKER_SUMMARY_VIEW_ICON_SIZE);
+const AttackCounterSummaryIcon = () => createImgComponentFromTag('<::attack-counter::>', MARKER_SUMMARY_VIEW_ICON_SIZE);
+const ShieldCounterSummaryIcon = () => createImgComponentFromTag('<::shield-counter::>', MARKER_SUMMARY_VIEW_ICON_SIZE);
+const PopulationIncreaseCounterSummaryIcon = () => createImgComponentFromTag('<::population-increase-counter::>', MARKER_SUMMARY_VIEW_ICON_SIZE);
+const PopulationSacrificeCounterSummaryIcon = () => createImgComponentFromTag('<::population-sacrifice-counter::>', MARKER_SUMMARY_VIEW_ICON_SIZE);
+
+const ManalithTokenSummaryIcon = () => createImgComponentFromTag('<::mana-level-token::>', MARKER_SUMMARY_VIEW_ICON_SIZE);
+const SoulstainTokenSummaryIcon = () => createImgComponentFromTag('<::soulstain-token::>', MARKER_SUMMARY_VIEW_ICON_SIZE);
+const PopulationTokenSummaryIcon = () => createImgComponentFromTag('<::population-token::>', MARKER_SUMMARY_VIEW_ICON_SIZE);
+
+const ManawaveRoundTokenSummaryIcon = () => createImgComponentFromTag('<::manawave-round-token::>', MARKER_SUMMARY_VIEW_ICON_SIZE);
+
+
+const ColorIcon = (props: {colorHex: string}) => {
+  return (
+    <div style={{
+        height: MARKER_SUMMARY_COLOR_BLOCK_SIZE,
+        width: MARKER_SUMMARY_COLOR_BLOCK_SIZE,
+        backgroundColor: props.colorHex,
+        borderRadius: 8,
+      }}
+      />
+  )
+}
+
+const GridItemLabel = (props: {children: ReactNode}) => {
+  return (
+    <div className="mw-markers-summary-grid-item-label">
+      {props.children}
+    </div>
+  )
+}
+
+const GridItem = (props: {children: ReactNode}) => {
+  return (
+    <div className="mw-markers-summary-grid-item">
+      {props.children}
+    </div>
+  )
+}
+
+
+export const MwMarkersSummarySheet = () => {
 
   return (
     <div className="mw-markers-summary-sheet">
@@ -13,66 +60,23 @@ export const MwMarkersSummarySheet = () => {
         </div>
 
         <div className="mw-markers-summary-grid">
-          <div>Name</div><div>Icon</div><div>Color</div>
+          {/* <GridItem>Name</GridItem><GridItem>Icon</GridItem><GridItem>Color</GridItem> */}
 
-          <div>Manalith Token</div><div>Icon</div><div>Color</div>
-          <div>Population Token</div><div>Icon</div><div>Color</div>
-          <div>Soulstain Token</div><div>Icon</div><div>Color</div>
+          <GridItemLabel>Manalith Token</GridItemLabel><GridItem><ManalithTokenSummaryIcon /></GridItem><GridItem><ColorIcon colorHex={HEX_COLOR_CODE_MANALITH_TOKEN} /></GridItem>
+          <GridItemLabel>Population Token</GridItemLabel><GridItem><PopulationTokenSummaryIcon /></GridItem><GridItem><ColorIcon colorHex={HEX_COLOR_CODE_POPULATION_TOKEN} /></GridItem>
+          <GridItemLabel>Soulstain Token</GridItemLabel><GridItem><SoulstainTokenSummaryIcon /></GridItem><GridItem><ColorIcon colorHex={HEX_COLOR_CODE_SOULSTAIN_TOKEN} /></GridItem>
 
-          <div>Attack Counter</div><div>Icon</div><div>Color</div>
-          <div>Shield Counter</div><div>Icon</div><div>Color</div>
-          <div>Mana Counter</div><div>Icon</div><div>Color</div>
-          <div>Manalith Usage Indicator</div><div>Icon</div><div>Color</div>
-          <div>Population Increase Counter</div><div>Icon</div><div>Color</div>
-          <div>Population Sacrifice Counter</div><div>Icon</div><div>Color</div>
+          <GridItemLabel>Attack Counter</GridItemLabel><GridItem><AttackCounterSummaryIcon /></GridItem><GridItem><ColorIcon colorHex={HEX_COLOR_CODE_ATTACK_COUNTER} /></GridItem>
+          <GridItemLabel>Shield Counter</GridItemLabel><GridItem><ShieldCounterSummaryIcon /></GridItem><GridItem><ColorIcon colorHex={HEX_COLOR_CODE_SHIELD_COUNTER} /></GridItem>
+          <GridItemLabel>Mana Counter</GridItemLabel><GridItem><ManaCounterSummaryIcon /></GridItem><GridItem><ColorIcon colorHex={HEX_COLOR_CODE_MANA_COUNTER} /></GridItem>
+          {/* <GridItemLabel>Manalith Usage Indicator</GridItemLabel><GridItem><ManalithTokenSummaryIcon /></GridItem><GridItem><ColorIcon colorHex={HEX_COLOR_CODE_MANALITH_TOKEN} /></GridItem> */}
+          <GridItemLabel>Pop. Increase Counter</GridItemLabel><GridItem><PopulationIncreaseCounterSummaryIcon /></GridItem><GridItem><ColorIcon colorHex={HEX_COLOR_CODE_POPULATION_INCREASE_COUNTER} /></GridItem>
+          <GridItemLabel>Pop. Sacrifice Counter</GridItemLabel><GridItem><PopulationSacrificeCounterSummaryIcon /></GridItem><GridItem><ColorIcon colorHex={HEX_COLOR_CODE_POPULATION_SACRIFICE_COUNTER} /></GridItem>
 
-          <div>Manawave Round Token</div><div>Icon</div><div>Color</div>
-
-{/*           
-          {
-            markerItems.map((item, i) => {
-              return (
-                <div 
-                  className='mw-markers-summary-grid-item'
-                  key={i}
-                  >
-                    Item {i}
-                </div>
-              )
-            })
-          } */}
+          <GridItemLabel>Manawave Round Token</GridItemLabel><GridItem><ManawaveRoundTokenSummaryIcon /></GridItem><GridItem><ColorIcon colorHex={HEX_COLOR_CODE_MANAWAVE_ROUND_TOKEN} /></GridItem>
         </div>
-      {/* {items.map((item) => (
-        <GridItem key={item}>Item {item}</GridItem>
-      ))}
-    </GridContainer> */}
-        {/* <div style={{fontSize: 16}}>
-
-          <ul>
-            <li>Increase Round Counter</li>
-            <li>Select Family/Bloodline</li>
-            <li>Place Family and Change Modes</li>
-            <li>Add Family Populations</li>
-            <li>Channel Clan Manaflows</li>
-            <li>Purchase and Place Counters/Tokens</li>
-            <li>Wage Clan Conflicts</li>
-            <li>Power Monuments</li>
-            <li>Consolidate Clan Counters</li>
-            <li>Restart Manawave Execution</li>
-          </ul>
-
-        </div> */}
       </div>
     </div>
   );
 };
 
-
-// export const MwMarkersSummarySheet = () => {
-//   return (
-//     <MwMarkersSummaryContainerDiv
-//       >
-//       Manawave Markers
-//     </MwMarkersSummaryContainerDiv>
-//   )
-// }
